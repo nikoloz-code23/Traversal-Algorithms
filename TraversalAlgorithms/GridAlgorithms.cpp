75c5764b56223da36050b802cf269601a4e8fcc4
nikoloz-code23 <literallynickk@gmail.com>
2025-12-05 17:40:52 +0400

    Cleanup of header and implementation of grid files. Still in progress.

--- TraversalAlgorithms/GridAlgorithms.cpp
+++ TraversalAlgorithms/GridAlgorithms.cpp
@@ -1,66 +1,76 @@
 #include "grid.h"
+#include <cstdint>
 #include <iostream>
+#include <climits>
+#include <algorithm>
+
+#define WALL -1
+#define STRAIGHT 10
+#define DIAGONAL 14
 
 void WeightedGrid::initGrid(unsigned int width, unsigned int height){
-    for(int i = 0; i < height; i++){
-        for(int j = 0; j < width; j++){
-            adjGrid[i].push_back(INT_MAX);
+    for(unsigned int i = 0; i < height; i++){
+        for(unsigned int j = 0; j < width; j++){
+            adjGrid[i].push_back(INT8_MAX);
         }
     }
 }
 
 void WeightedGrid::setWall(unsigned int x, unsigned int y){
-    adjGrid[y][x] = -1;
+    adjGrid[y][x] = WALL;
 }
 
-void WeightedGrid::pushNeighbour(int &vertexX, int &vertexY, int &cost,
+void WeightedGrid::pushNeighbour(int &vertexX, int &vertexY, 
         std::priority_queue<std::pair<int, std::pair<unsigned int, unsigned int> >,
                         std::vector< std::pair<int, std::pair<unsigned int, unsigned int> > >,
                         std::greater< std::pair<int, std::pair<unsigned int, unsigned int> > >
     > &pq){
-        //////////// NOT DIAGONAL ///////////
 
-        if( (vertexX-1) >= 0 && adjGrid[vertexY][vertexX]+10 < adjGrid[vertexY][vertexX-1]){
-            adjGrid[vertexY][vertexX-1] = adjGrid[vertexY][vertexX]+10;
+        // TODO: Find a way to shorten and clean up this function.
+
+        ////////// NOT DIAGONAL ///////////
+
+        if( (vertexX-1) >= 0 && adjGrid[vertexY][vertexX]+STRAIGHT < adjGrid[vertexY][vertexX-1]){
+            adjGrid[vertexY][vertexX-1] = adjGrid[vertexY][vertexX]+STRAIGHT;
             pq.push(std::make_pair(adjGrid[vertexY][vertexX-1], std::make_pair(vertexY,vertexX-1)));
             previousCell[{vertexY,vertexX-1}] = {vertexY,vertexX};
         }
-        if( (vertexY-1) >= 0 && adjGrid[vertexY][vertexX]+10 < adjGrid[vertexY-1][vertexX]){
-           adjGrid[vertexY-1][vertexX] = adjGrid[vertexY][vertexX]+10;
+        if( (vertexY-1) >= 0 && adjGrid[vertexY][vertexX]+STRAIGHT < adjGrid[vertexY-1][vertexX]){
+           adjGrid[vertexY-1][vertexX] = adjGrid[vertexY][vertexX]+STRAIGHT;
            pq.push(std::make_pair(adjGrid[vertexY-1][vertexX], std::make_pair(vertexY-1,vertexX)));
            previousCell[{vertexY-1,vertexX}] = {vertexY,vertexX};
         }
-        if( (vertexX+1) < 8 && adjGrid[vertexY][vertexX]+10 < adjGrid[vertexY][vertexX+1]){
-            adjGrid[vertexY][vertexX+1] = adjGrid[vertexY][vertexX]+10;
+        if( (vertexX+1) < 8 && adjGrid[vertexY][vertexX]+STRAIGHT < adjGrid[vertexY][vertexX+1]){
+            adjGrid[vertexY][vertexX+1] = adjGrid[vertexY][vertexX]+STRAIGHT;
             pq.push(std::make_pair(adjGrid[vertexY][vertexX+1], std::make_pair(vertexY,vertexX+1)));
             previousCell[{vertexY,vertexX+1}] = {vertexY,vertexX};
         }
-        if( (vertexY+1) < 8 && adjGrid[vertexY][vertexX]+10 < adjGrid[vertexY+1][vertexX]){
-            adjGrid[vertexY+1][vertexX] = adjGrid[vertexY][vertexX]+10;
+        if( (vertexY+1) < 8 && adjGrid[vertexY][vertexX]+STRAIGHT < adjGrid[vertexY+1][vertexX]){
+            adjGrid[vertexY+1][vertexX] = adjGrid[vertexY][vertexX]+STRAIGHT;
             pq.push(std::make_pair(adjGrid[vertexY+1][vertexX], std::make_pair(vertexY+1,vertexX)));
             previousCell[{vertexY+1,vertexX}] = {vertexY,vertexX};
         }
 
         //////////// DIAGONAL ///////////
 
-        if( (vertexX-1) >= 0 && (vertexY-1) >= 0 && adjGrid[vertexY][vertexX]+14 < adjGrid[vertexY-1][vertexX-1]){
-            adjGrid[vertexY-1][vertexX-1] = adjGrid[vertexY][vertexX]+14;
+        if( (vertexX-1) >= 0 && (vertexY-1) >= 0 && adjGrid[vertexY][vertexX]+DIAGONAL < adjGrid[vertexY-1][vertexX-1]){
+            adjGrid[vertexY-1][vertexX-1] = adjGrid[vertexY][vertexX]+DIAGONAL;
             pq.push(std::make_pair(adjGrid[vertexY-1][vertexX-1], std::make_pair(vertexY-1,vertexX-1)));
             previousCell[{vertexY-1,vertexX-1}] = {vertexY,vertexX};
         }
-        if( (vertexX-1) >= 0 && (vertexY+1) < 8 && adjGrid[vertexY][vertexX]+14 < adjGrid[vertexY+1][vertexX-1]){
-            adjGrid[vertexY+1][vertexX-1] = adjGrid[vertexY][vertexX]+14;
+        if( (vertexX-1) >= 0 && (vertexY+1) < 8 && adjGrid[vertexY][vertexX]+DIAGONAL < adjGrid[vertexY+1][vertexX-1]){
+            adjGrid[vertexY+1][vertexX-1] = adjGrid[vertexY][vertexX]+DIAGONAL;
             pq.push(std::make_pair(adjGrid[vertexY+1][vertexX-1], std::make_pair(vertexY+1,vertexX-1)));
             previousCell[{vertexY+1,vertexX-1}] = {vertexY,vertexX};
         }
 
-        if( (vertexX+1) < 8 && (vertexY-1) >= 0 && adjGrid[vertexY][vertexX]+14 < adjGrid[vertexY-1][vertexX+1]){
-            adjGrid[vertexY-1][vertexX+1] = adjGrid[vertexY][vertexX]+14;
+        if( (vertexX+1) < 8 && (vertexY-1) >= 0 && adjGrid[vertexY][vertexX]+DIAGONAL < adjGrid[vertexY-1][vertexX+1]){
+            adjGrid[vertexY-1][vertexX+1] = adjGrid[vertexY][vertexX]+DIAGONAL;
             pq.push(std::make_pair(adjGrid[vertexY-1][vertexX+1], std::make_pair(vertexY-1,vertexX+1)));
             previousCell[{vertexY-1,vertexX+1}] = {vertexY,vertexX};
         }
-        if( (vertexX+1) < 8 && (vertexY+1) < 8 && adjGrid[vertexY][vertexX]+14 < adjGrid[vertexY+1][vertexX+1]){
-            adjGrid[vertexY+1][vertexX+1] = adjGrid[vertexY][vertexX]+14;
+        if( (vertexX+1) < 8 && (vertexY+1) < 8 && adjGrid[vertexY][vertexX]+DIAGONAL < adjGrid[vertexY+1][vertexX+1]){
+            adjGrid[vertexY+1][vertexX+1] = adjGrid[vertexY][vertexX]+DIAGONAL;
             pq.push(std::make_pair(adjGrid[vertexY+1][vertexX+1], std::make_pair(vertexY+1,vertexX+1)));
             previousCell[{vertexY+1,vertexX+1}] = {vertexY,vertexX};
         }
@@ -69,8 +79,7 @@ void WeightedGrid::pushNeighbour(int &vertexX, int &vertexY, int &cost,
 void WeightedGrid::dijkstraGrid(unsigned int x, unsigned int y){
     visitedCell.clear();
 
-
-    adjGrid[y][x] = 0; // set start
+    adjGrid[y][x] = 0; // The start position will have the lowest weight. 
     startX = x;
     startY = y;
 
@@ -88,9 +97,8 @@ void WeightedGrid::dijkstraGrid(unsigned int x, unsigned int y){
         int vertexX = current_vertex.second;
         pq.pop();
 
-        if(visitedCell.find(current_vertex) != visitedCell.end()){
+        if(visitedCell.find(current_vertex) != visitedCell.end())
             continue;
-        }
 
         if(cost == -1)
             continue;
@@ -99,22 +107,21 @@ void WeightedGrid::dijkstraGrid(unsigned int x, unsigned int y){
 
         visitedCell.insert(std::make_pair(vertexY, vertexX));
 
-        pushNeighbour(vertexX, vertexY, cost, pq);
+        pushNeighbour(vertexX, vertexY, pq);
     }
 }
 
 void WeightedGrid::printGrid(unsigned int width, unsigned int height){
-    for(int i = 0; i < height; i++){
+    for(unsigned int i = 0; i < height; i++){
         std::cout<<'\n';
-        for(int j = 0; j < width; j++){
+        for(int j = 0; j < width; j++)
             std::cout<<adjGrid[i][j]<<' ';
-        }
     }
-    std::cout<<std::endl<<std::endl;
+    std::cout<<'\n'<<std::endl;
 }
 
 void WeightedGrid::findPath(unsigned int endX, unsigned int endY){
-    std::pair<unsigned int, unsigned int> start = {startY,startX};
+    std::pair<unsigned int, unsigned int> start = { startY, startX };
     std::pair<unsigned int, unsigned int> prev;
     std::vector<std::pair<unsigned int, unsigned int>> path;
 
@@ -130,7 +137,6 @@ void WeightedGrid::findPath(unsigned int endX, unsigned int endY){
 
     std::reverse(path.begin(), path.end());
 
-    for(auto const &cell : path){
+    for(auto const &cell : path)
         std::cout << cell.first << ' ' << cell.second << '\n';
-    }
 }
--- TraversalAlgorithms/grid.h
+++ TraversalAlgorithms/grid.h
@@ -5,22 +5,22 @@
 #include <vector>
 #include <utility>
 
+#define PRIORITY_QUEUE std::priority_queue<std::pair<int, std::pair<unsigned int, unsigned int> >, std::vector< std::pair<int, std::pair<unsigned int, unsigned int> > >,std::greater< std::pair<int, std::pair<unsigned int, unsigned int> > >>
+
 class WeightedGrid{
     private:
         unsigned int startX,startY;
 
-        std::unordered_map<unsigned int, std::vector<int>> adjGrid; //represent a grid like this
+        // Representation of the 2D Grid.
+        // The first parameter represents the Y axis. The second is an array of X coordinates.
+        std::unordered_map<unsigned int, std::vector<int>> adjGrid; 
+        // For our Dijkstra, every cell that we store will have a parent cell, to make a path.
         std::map< std::pair<unsigned int, unsigned int>, std::pair<unsigned int, unsigned int> > previousCell;
-        std::set<std::pair<unsigned int, unsigned int>> visitedCell; //save visited data
+        std::set<std::pair<unsigned int, unsigned int>> visitedCell;
     public:
         void initGrid(unsigned int width, unsigned int height);
         void setWall(unsigned int x, unsigned int y);
-        void pushNeighbour(int &vertexX, int &vertexY, int &cost,
-        std::priority_queue<std::pair<int, std::pair<unsigned int, unsigned int> >,
-                        std::vector< std::pair<int, std::pair<unsigned int, unsigned int> > >,
-                        std::greater< std::pair<int, std::pair<unsigned int, unsigned int> > >
-                        > &pq
-        );
+        void pushNeighbour(int &vertexX, int &vertexY, PRIORITY_QUEUE &pq);
         void dijkstraGrid(unsigned int x, unsigned int y);
         void printGrid(unsigned int width, unsigned int height);
         void findPath(unsigned int endX, unsigned int endy);
